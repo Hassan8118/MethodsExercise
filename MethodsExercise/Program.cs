847d3d569c2aad3bceffe086b7e535486621662a
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-10 12:54:54 -0400

    MethodsExercisedone

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("who is your favorite rapper");
+            string rapper = Console.ReadLine();
+
+            Console.WriteLine($"There was a man named {userName}, his favorite color is {color}.");
+            Console.WriteLine($"His favorite animal is a {animal}, and his favorite rapper is {rapper}.");
+
+            Console.WriteLine(Sum(5, 5));
+            Console.WriteLine(Multiply(5, 5));
+            Console.WriteLine(Subtracting(9,8));
+            Console.WriteLine(Divide(14, 7));
+
+        }
+        public static int Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+
+
+            return num1 + num2;
+        }
+        public static int Multiply(int x, int y)
+        {
+
+
+            return x * y;
+
+        }
+        public static int Subtracting(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Divide(int x, int y)
+        {
+            return x / y;
         }
     }
 }
